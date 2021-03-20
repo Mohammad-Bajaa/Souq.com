00d920868fb956c1a43a32f67084c095ed57674b
alaDDin101 <frq.ad1@gmail.com>
2021-03-20 13:05:23 +0200

    الله يريحنا ع اهون سبب

--- Souq/src/Souq.Web/Controllers/HomeController.cs
+++ Souq/src/Souq.Web/Controllers/HomeController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Clean.Architecture.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace Souq.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly StoreDbContext _context;
+
+        public HomeController(StoreDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        public async Task<IActionResult> CategoriesItems()
+        {
+
+
+            return View(await _context.Categories.ToListAsync());
         }
 
         public IActionResult Error()
