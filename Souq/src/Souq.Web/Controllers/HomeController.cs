a8c7405620282f2921063c7744f78a8353b1cb8b
Mohammad-Bajaa <75749165+Mohammad-Bajaa@users.noreply.github.com>
2021-03-21 19:45:02 +0200

    Creating Cart with some Fixes

--- Souq/src/Souq.Web/Controllers/HomeController.cs
+++ Souq/src/Souq.Web/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using Clean.Architecture.Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace Souq.Web.Controllers
 {
@@ -17,11 +19,20 @@ namespace Souq.Web.Controllers
         {
             return View();
         }
-        public async Task<IActionResult> CategoriesItems()
+        public IActionResult HomePage()
         {
+            return View();
+        }
+        public async Task<IActionResult> CategoriesItems(int page_number =1)
+        {   var skip = (page_number - 1) * 6;
+            var take = 6;
+            
+            ViewBag.Items =  _context.Items.ToList().ToPagedList(page_number, 3);
+            ViewBag.Categories = await _context.Categories.ToListAsync();
+            ViewBag.page_number = page_number;
+         
 
-
-            return View(await _context.Categories.ToListAsync());
+            return View();
         }
 
         public IActionResult Error()
